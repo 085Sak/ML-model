47286041dafecdfbd58d626cceeb68a4fd17c295
085Sak <76028688+085Sak@users.noreply.github.com>
2021-05-16 14:06:20 +0530

    Update salaryprediction.py

--- salaryprediction.py
+++ salaryprediction.py
@@ -1,120 +1,111 @@
-import os
+#from typing import ValuesView
+from matplotlib import colors
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn import train_test_split
+import os
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 def welcome():
-    print("WELCOME! TO SLARY PEDICTION SYSTEM")
-    print("Press any key")
+    print("Welcome to Salary Prediction System")
+    print("Press Enter key to proceed")
     input()
 def checkcsv():
     csv_files=[]
-    cur_dir=os.getcwd()
+    cur_dir=os.getcwd()#give current directory
     content_list=os.listdir(cur_dir)
-    for x in content_list:
-        if x.split(".")[-1]=="csv":
-            csv_files.append(x)
-    if len(csv_files)==0:
-        return " Oops something went wrong  no csv file  there"
-    else:
-       return csv_files
+    print(content_list)
+    csv_files=content_list
+    
+    return csv_files
 def display_and_select_csv(csv_files):
     i=0
     for file_name in csv_files:
-        print(i,"...",file_name)
+        print(i,"....",file_name)
         i+=1
-    return csv_files[int(input("select any file"))]
-def graph(x_train,y_train,regressionObject,x_test,y_test,y_pred):
-    plt.scatter(x_train,y_train,color="red",label="Training Data")
-    plt.plot(x_tarin,regressionObject.predict(x_train),color="blue",label="BestFit")
-    plt.scatter(x_test,y_test,color="green",label="test data")
-    plt.scatter(x_test,y_pred,color="black",label="predicted test data")
-    plt.title("salary Vs experience")
-    plt.xlabel("Years of experience")
+    return csv_files[int(input("Select required csv file :  "))]      
+def graph(X_train,Y_train,regressionObject,X_test,Y_test,Y_pred):
+    plt.scatter(X_train,Y_train,color="red",label="training data")
+  
+   # print(Y_train)
+    #print(regressionObject.predict(X_train))
+
+   
+    plt.plot(X_train,regressionObject.predict(X_train),color="Blue",label="best fit")
+    plt.scatter(X_test,Y_test,color='green',label="test data")
+    plt.scatter(X_test,Y_pred,color="black",label="preicted test data")
+    plt.title("Salary vs Experience")
+    plt.xlabel("Experience")
     plt.ylabel("Salary")
     plt.legend()
-    plt.show()
-              
-    
-                     
-    
-
+    plt.show()            
 def main():
     welcome()
+    #print("helo!")
     try:
-        csv_files=checkcsv()
-        if csv_files==" Oops! something went wrong  no csv file  there":
+        csv_files=checkcsv()# function to check how many csv files are there in directory
+        if(csv_files=="NO CSV FILE FOUND!"):
             raise FileNotFoundError("No csv file in the directory")
-        csv_file=display_and_select_csv(csv_files)
+        csv_file=display_and_select_csv(csv_files)    
         print(csv_file,"is selected")
-        print("readig cdv file.....")
-        print("crearing dataset...")
+        print("Reading csv file...")
+        print("Creating data set")
         dataset=pd.read_csv(csv_file)
-        print("Data set created")
-        x=dataset.iloc[:,:-1].values
-        y=dataset.iloc[:,-1].values
-        s=float(input("Enter test data size between ( 0 and 1) "))
-        x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=s)#return 4 values
-        print("Model! Creation In progress")
+        print("data set created")
+        X=dataset.iloc[:,:-1].values
+        Y=dataset.iloc[:,-1].values
+        s=float(input("enter test data size between 0 and 1:"))#0.1=10 percent
+        X_train,X_test,Y_train,Y_test= train_test_split(X,Y,test_size=s)
+        print("Model creation in progress....")
         regressionObject=LinearRegression()
-        regressionObject.fit(x_train,y_train)
-        print("Model! Is Created")
-        print("Press Enter key to predict test data")
+        regressionObject.fit(X_train,Y_train)# for regression line on the basis of training data best fit
+        print("Model created!")
+        print("press Enter key to predict test data")
         input()
-        y_pred=regressionObject.predict(x_test)
+        Y_pred=regressionObject.predict(X_test)# predict Y with the help of X predict is a function
         i=0
-        print(x-testdata,"....",y-testdata,".....",y-predicted-data)
-        while(i<len(x_test)):
-              print(x_test[i],"......",y_test[i],".....",y_pred[i])
-              i+=1
-        print("Press Enter  Key To See Above Result In Graphical Format")
+        print('X_test,"....",Y_test,"....",Y_pred')
+        while i<len(X_test):
+            print(X_test[i],"....",Y_test[i],"....",Y_pred[i])
+            i+=1
+        print("Press Enter key to see above result in graphical format")
         input()
-        graph(x_train,y_train,regressionObject,x_test,y_test,y_pred)
-        r2=r2_score(y_test,y_pred)
-        print("Our model is %2.2f%% accurate " %(r2*100))
-        print("Now You can Predict Salary ")
-        print("\n Enter experience in years separated by comma")
-        exp=[float(e) for e in input().split(",")]
+        graph(X_train,Y_train,regressionObject,X_test,Y_test,Y_pred)
+        r2=r2_score(Y_test,Y_pred)
+        print("Our model is %2.2f%%   accurate" %(r2*100))
+
+        print("You can predict salary of an employee ")
+        print("enter experience in years of the candidates separated by comma")
+        exp=[float(l) for l in input().split(',')]
         ex=[]
-        for  x in exp:
-              ex.append([x])
+        for x in exp:
+            ex.append([x])#every elemnt is list
         experience=np.array(ex)
         salaries=regressionObject.predict(experience)
-              
-              
-        plt.scatter(experience,salaries,color="balck")
-        plt.xlabel("Years Of Experience")
+        plt.scatter(experience,salaries,color='black')
+        plt.xlabel("years of experience")
         plt.ylabel("Salaries")
         plt.show()
-        d=pd.DataFrame({"Experience":exp,"Salaries":salaries})
-        print(d)      
-        
-        
-        
-        
-                         
-            
-        
-        
-        
-        
-        
+        d=pd.DataFrame({"Experience":exp,"Salaries": salaries})    
+        print(d)
+
+
+              
+     
+    
     except FileNotFoundError:
-        print("No csv file in the directory")
-        print("Press Enter Key To Exit")
+        print("NO csv file in the directory")
+        print("press enter key to exit")
         input()
         exit()
-        
-
-
-
-
-if __name__=="__main__":
-
-    main()
-    input()
-
 
     
+    
+if __name__=="__main__":
+    main()
+    input()
+    
+    # ek ki press krne pr hi program end ho
+    
+
